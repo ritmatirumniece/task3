a6c40a6b0f636e576ca92f3fbf0e42b7450a5c97
Ritma <ritma.tirumniece@gmail.com>
2022-09-10 19:37:41 +0300

    changes

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ computerScoreSelector.innerHTML = computerScore;
 scissorsButton.addEventListener("click", () => {
   playerSelection = "SCISSORS";
   console.log(playerSelection);
-  game();
+  playRound(playerSelection);
   if (playerScore === 5) {
     result.innerHTML = "The game is over! You win!";
     console.log(result.innerHTML);
@@ -30,7 +30,7 @@ scissorsButton.addEventListener("click", () => {
 paperButton.addEventListener("click", () => {
   playerSelection = "PAPER";
   console.log(playerSelection);
-  game();
+  playRound(playerSelection);
   if (playerScore === 5) {
     result.innerHTML = "The game is over! You win!";
     console.log(result.innerHTML);
@@ -43,7 +43,7 @@ paperButton.addEventListener("click", () => {
 rockButton.addEventListener("click", () => {
   playerSelection = "ROCK";
   console.log(playerSelection);
-  game();
+  playRound(playerSelection);
   if (playerScore === 5) {
     result.innerHTML = "The game is over! You win!";
     console.log(result.innerHTML);
@@ -58,42 +58,42 @@ const game = () => {
   } else if (playerSelection === "PAPER") {
   } else if (playerSelection === "ROCK") {
   }
-
-  // function computerPlay() {
-  //   const selection = ["rock", "paper", "scissors"];
-  //   return selection[Math.floor(Math.random() * selection.length)];
-  // }
-
-  let computerSelection = Math.floor(Math.random() * 3 + 1);
-  if (computerSelection === 1) {
-    computerSelection === "scissors";
-    console.log("Computer: " + computerSelection);
-  } else if (computerSelection === 2) {
-    computerSelection === "paper";
-    console.log("Computer: " + computerSelection);
-  } else if (computerSelection === 3) {
-    computerSelection === "rock";
-    console.log("Computer: " + computerSelection);
-  }
-  playRound(playerSelection, computerSelection);
-  return;
 };
+function computerSelection() {
+  const selection = ["rock", "paper", "scissors"];
+  return selection[Math.floor(Math.random() * selection.length)];
+}
 
-const playRound = (playerSelection, computerSelection) => {
+// let computerSelection = Math.floor(Math.random() * 3 ;
+//   if (computerSelection === 1) {
+//     computerSelection  = "scissors";
+//     console.log("Computer: " + computerSelection);
+//   } else if (computerSelection === 2) {
+//     computerSelection ="paper";
+//     console.log("Computer: " + computerSelection);
+//   } else if (computerSelection === 3) {
+//     computerSelection ="rock";
+//     console.log("Computer: " + computerSelection);
+//   }
+//   playRound(playerSelection, computerSelection);
+//   return;
+// };
+
+function playRound(playerSelection) {
+  computerSelection = computerSelection();
   if (playerSelection === computerSelection) {
-    result.innerHTML = ` It is draw! No one wins! You both have picked ${playerSelection}`;
+    result.innerText = ` It is draw! No one wins! You both have picked ${playerSelection}`;
   } else if (
     (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
     (playerSelection === "PAPER" && computerSelection === "ROCK") ||
     (playerSelection === "SCISSORS" && computerSelection === "PAPER")
   ) {
     playerScore++;
-    playerScoreSelector.innerHTML = playerScore;
-    result.innerHTML = `You win! ${playerSelection} beats ${computerSelection}!`;
+    playerScoreSelector.innerText = playerScore;
+    result.innerText = `You win! ${playerSelection} beats ${computerSelection}!`;
   } else {
     computerScore++;
-    computerScoreSelector.innerHTML = computerScore;
-    result.innerHTML = `You lose! ${computerSelection} beats ${playerSelection}!`;
+    computerScoreSelector.innerText = computerScore;
+    result.innerText = `You lose! ${computerSelection} beats ${playerSelection}!`;
   }
-  return;
-};
+}
