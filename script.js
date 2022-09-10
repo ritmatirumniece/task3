8371c18aae242c58705dca6e45f6bd7254750b62
Ritma <ritma.tirumniece@gmail.com>
2022-09-10 15:32:54 +0300

    changes

--- script.js
+++ script.js
@@ -8,67 +8,92 @@ const computerScoreSelector = document.querySelector('#computerScore');
 const result = document.querySelector('#result');
 
 let playerSelection;
-const scissorsButton = document.querySelector('scissorsButton');
-const paperButton = document.querySelector('paperButton');
-const rockButton = document.querySelector('rockButton');
+const scissorsButton = document.querySelector('#scissorsButton');
+const paperButton = document.querySelector('#paperButton');
+const rockButton = document.querySelector('#rockButton');
 
 playerScoreSelector.innerHTML = playerScore;
 computerScoreSelector.innerHTML = computerScore;
 
+
 scissorsButton.addEventListener('click', () =>{
-  playerSelection='scissors';
-  // console.log(playerSelection)
+  playerSelection='SCISSORS';
+  console.log(playerSelection)
   game();
-  if(playerScore === 5 || computerScore === 5) {
-    result.innerHTML = 'The game is over!';
-    // console.log(result.innerHTML);
+  if(playerScore === 5) {
+    result.innerHTML = 'The game is over! You win!';
+    console.log(result.innerHTML);
+  } else if (computerScore ===5) {
+    result.innerHTML = 'The game is over! Computer wins!';
+    console.log(result.innerHTML);
   }
 });
 
 paperButton.addEventListener('click', () =>{
-  playerSelection='paper';
-  // console.log(playerSelection)
+  playerSelection='PAPER';
+  console.log(playerSelection)
   game();
-  if(playerScore === 5 || computerScore === 5) {
-    result.innerHTML = 'The game is over!';
-    // console.log(result.innerHTML);
+  if(playerScore === 5) {
+    result.innerHTML = 'The game is over! You win!';
+    console.log(result.innerHTML);
+  } else if (computerScore ===5) {
+    result.innerHTML = 'The game is over! Computer wins!';
+    console.log(result.innerHTML);
   }
 });
 
 rockButton.addEventListener('click', () =>{
-  playerSelection='rock';
-  // console.log(playerSelection)
+  playerSelection='ROCK';
+  console.log(playerSelection)
   game();
-  if(playerScore === 5 || computerScore === 5) {
-    result.innerHTML = 'The game is over!';
-    // console.log(result.innerHTML);
+  if(playerScore === 5) {
+    result.innerHTML = 'The game is over! You win!';
+    console.log(result.innerHTML);
+  } else if (computerScore ===5) {
+    result.innerHTML = 'The game is over! Computer wins!';
+    console.log(result.innerHTML);
   }
 });
 
 
 
 const game = () => {
-  if(playerSelection === 'scissors') {
-  } else if(playerSelection === 'paper') {
-  } else if(playerSelection === 'rock') {
+  if(playerSelection === 'SCISSORS') {
+  } else if(playerSelection === 'PAPER') {
+  } else if(playerSelection === 'ROCK') {
   }
 
-  let computerSelection = [Math.floor(Math.random() * selection.length)];
-  if (computerSelection === 'scissors') {
-  } else if (computerSelection === 'paper'){
-  } else if (computerSelection === 'rock'){
+  // function computerPlay() {
+  //   const selection = ["rock", "paper", "scissors"];
+  //   return selection[Math.floor(Math.random() * selection.length)];
+  // }
+
+
+
+  let computerSelection = Math.floor((Math.random() * 3) +1);
+  if (computerSelection === 1) {
+    computerSelection === 'scissors';
+    console.log('Computer: ' + computerSelection);
+  } else if (computerSelection === 2) {
+    computerSelection === 'paper';
+    console.log('Computer: ' + computerSelection);
+  } else if (computerSelection === 3) {
+    computerSelection === 'rock';
+    console.log('Computer: ' + computerSelection);
   }
   playRound(playerSelection, computerSelection);
   return;
 }
+  
+
 
 const playRound = (playerSelection, computerSelection) =>{
   if (playerSelection === computerSelection) {
     result.innerHTML = ` It is draw! No one wins! You both have picked ${playerSelection}`;
   } else if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
+    (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+    (playerSelection === "PAPER" && computerSelection === "ROCK") ||
+    (playerSelection === "SCISSORS" && computerSelection === "PAPER")
   ) {
     playerScore++;
     playerScoreSelector.innerHTML = playerScore;
@@ -78,14 +103,55 @@ const playRound = (playerSelection, computerSelection) =>{
     computerScoreSelector.innerHTML = computerScore;
     result.innerHTML = `You lose! ${computerSelection} beats ${playerSelection}!`;
   }
-  return
-};
+  return;
+}
 
 
-
-
-
-
-
-
-
+// steve part code
+// const playRound = (playerSelection, computerSelection) => {
+//   if (computerSelection === playerSelection) {
+//   // console.log(`It';s a tie! You both chose ${computerSelection}!`);
+//   result.innerHTML = `It's a tie! You both chose ${computerSelection}!`;
+//   } else if (computerSelection === 'rock') {
+//   if (playerSelection === 'scissors') {
+//   // console.log(`You lose because ${computerSelection} beats ${playerSelection}!`);
+//   computerScore = computerScore + 1;
+//   computerScoreSelector.innerHTML = computerScore;
+//   result.innerHTML = `You lose because ${computerSelection} beats ${playerSelection}!`;
+//   } else if (playerSelection === 'paper') {
+//   // console.log(`You win because ${playerSelection} beats ${computerSelection}!`);
+//   playerScore = playerScore + 1;
+//   playerScoreSelector.innerHTML = playerScore;
+//   result.innerHTML = `You win because ${playerSelection} beats ${computerSelection}!`;
+//   }
+//   } else if (computerSelection === 'paper') {
+//   if (playerSelection === 'rock') {
+//   // console.log(`You lose because ${computerSelection} beats ${playerSelection}!`);
+//   computerScore = computerScore + 1;
+//   computerScoreSelector.innerHTML = computerScore;
+//   result.innerHTML = `You lose because ${computerSelection} beats ${playerSelection}!`;
+//   } else if (playerSelection === 'scissors') {
+//   // console.log(`You win because ${playerSelection} beats ${computerSelection}!`);
+//   playerScore = playerScore + 1;
+//   playerScoreSelector.innerHTML = playerScore;
+//   result.innerHTML = `You win because ${playerSelection} beats ${computerSelection}!`;
+//   }
+//   } else if (computerSelection === 'scissors') {
+//   if (playerSelection === 'rock') {
+//   // console.log(`You win because ${playerSelection} beats ${computerSelection}!`);
+//   playerScore = playerScore + 1;
+//   playerScoreSelector.innerHTML = playerScore;
+//   result.innerHTML = `You win because ${playerSelection} beats ${computerSelection}!`;
+//   } else if (playerSelection === 'paper') {
+//   // console.log(`You lose because ${computerSelection} beats ${playerSelection}!`);
+//   computerScore = computerScore + 1;
+//   computerScoreSelector.innerHTML = computerScore;
+//   result.innerHTML = `You lose because ${computerSelection} beats ${playerSelection}!`;
+//   }
+//   // } else {
+//   // // console.log(&quot;Please make a valid selection!&quot;);
+//   // result.innerHTML = `Please make a valid selection!`;
+//   // }
+//   return;
+//   }
+// }
